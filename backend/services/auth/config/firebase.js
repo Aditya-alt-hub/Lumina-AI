dabe671956c22083c5d6e754ece56e24013f90eb
Aditya <adityabhadauria63@gmail.com>
2026-09-09 03:18:34 +0530

    Fix Firebase credentials for deployment

--- backend/services/auth/config/firebase.js
+++ backend/services/auth/config/firebase.js
@@ -1,8 +1,12 @@
-
 import { cert, initializeApp } from "firebase-admin";
+import dotenv from "dotenv";
+dotenv.config()
 
-import serviceAccount from "../serviceAccountKey.json" with {type:"json"};
 
-export const app=initializeApp({
-  credential: cert(serviceAccount)
+export const app = initializeApp({
+    credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+    })
 });
