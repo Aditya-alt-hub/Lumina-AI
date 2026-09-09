a29ccf778aa18d06f433116cff883daff8f46642
Aditya <adityabhadauria63@gmail.com>
2026-09-09 14:16:32 +0530

    fix production session cookie

--- backend/services/auth/controllers/auth.controller.js
+++ backend/services/auth/controllers/auth.controller.js
@@ -46,7 +46,7 @@ export const login=async (req,res)=>
 
          res.cookie("session",sessionId,{
             httpOnly:true,
-            secure:false,
+            secure:true,
             sameSite:"none",
             // sameSite:"lax",
             maxAge:7*24*60*60*1000
